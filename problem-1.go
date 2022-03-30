8b47a471b5133dab32ecc6bf4aedd9fcf66a6fb5
user-penguin <kobzev96@gmail.com>
2022-03-31 00:52:42 +0700

    simple example

--- problem-1.go
+++ problem-1.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Human struct {
+	firstName string
+	lastName  string
+	thirdName string
+	age       int
+	height    int
+	weight    int
+}
+
+func (h *Human) GetFullName() string {
+	return h.lastName + " " + h.firstName + " " + h.thirdName
+}
+
+type Action struct {
+	name string
+	Human
+}
+
+func main() {
+	action := Action{
+		name: "test-1",
+		Human: Human{
+			firstName: "Dmitry",
+			lastName:  "Kobzev",
+			thirdName: "Igorevich",
+			age:       25,
+			height:    188,
+			weight:    85,
+		},
+	}
+	fmt.Printf("Full name of action's human: %s", action.GetFullName())
 }
