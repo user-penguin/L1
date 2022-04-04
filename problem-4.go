391a757ad5b3005ba00b30045ce28c50c5ec963b
user-penguin <kobzev96@gmail.com>
2022-04-04 20:19:58 +0700

    work with interrupt

--- problem-4.go
+++ problem-4.go
@@ -1,15 +1,43 @@
+/*
+Задача 4.
+Реализовать постоянную запись данных в канал (главный поток).
+Реализовать набор из N воркеров, которые читают произвольные данные из канала и выводят в stdout.
+Необходима возможность выбора количества воркеров при старте.
+
+Программа должна завершаться по нажатию Ctrl+C.
+Выбрать и обосновать способ завершения работы всех воркеров.
+*/
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
 	"sync"
-	"time"
+	"syscall"
 )
 
 func main() {
 	workerPoolSize := 10
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT)
+	go interruptControl(sigCh)
 	run(workerPoolSize)
+}
+
+func interruptControl(sigCh chan os.Signal) {
+	for {
+		s := <-sigCh
+		switch s {
+		case syscall.SIGINT:
+			fmt.Printf("Get ~C\n")
+			os.Exit(0)
+		default:
+			fmt.Printf("Unknown signal\n")
+		}
+
+	}
 }
 
 func run(workerPoolSize int) {
@@ -23,19 +51,19 @@ func run(workerPoolSize int) {
 			defer wg.Done()
 			for data := range dataCh {
 				fmt.Printf("worker(%d) data from channel: %s, len(%d) cap(%d)\n", workerNumber, data, len(dataCh), cap(dataCh))
-				time.Sleep(time.Millisecond * 1000)
+				//time.Sleep(time.Millisecond * 1000)
 			}
 		}()
 	}
 	wg.Wait()
-	close(dataCh)
 }
 
 func fillChannel(dataChan chan string) {
-	for {
+	for i := 0; i < 1000; i++ {
 		dataChan <- generateData()
-		time.Sleep(time.Millisecond * 1)
+		//time.Sleep(time.Millisecond * 1)
 	}
+	close(dataChan)
 }
 
 const alf = "qwertyuiopasdfghjklzxcvbnm1234567890"
