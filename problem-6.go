9e2b919237b007538e07582a81cab96cd030863c
user-penguin <kobzev96@gmail.com>
2022-04-05 21:47:50 +0700

    first method to stop goroutine

--- problem-6.go
+++ problem-6.go
@@ -4,6 +4,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	ch := make(chan int)
+	go goroutine1(ch)
+	time.Sleep(time.Second * 2)
+	ch <- 1
+	close(ch)
+}
+
+// первая горутина, остнавливаем каналом
+func goroutine1(ch chan int) {
+	for {
+		select {
+		case <-ch:
+			fmt.Println("Остановочка")
+			return
+		default:
+			fmt.Println("Метро люблино, работаем")
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
 }
