634c53538881e9875aac96d5218369265b79a173
user-penguin <kobzev96@gmail.com>
2022-04-08 04:13:02 +0700

    completed problem 17

--- p17/problem-17.go
+++ p17/problem-17.go
@@ -6,7 +6,7 @@ package p17
 import "fmt"
 
 func Run() {
-	source := []int{6, 1, 23, 33, -2, 5, 34, -15, 6, 2}
+	source := []int{-15, -2, 1, 2, 5, 6, 23, 33, 34}
 	find := 2
 	index := binarySearch(source, find)
 	if index >= 0 {
@@ -16,6 +16,19 @@ func Run() {
 	}
 }
 
+// бинарный поиск работает для упорядоченного массива!
 func binarySearch(src []int, find int) int {
+	first, last := 0, len(src)-1
+	for first <= last {
+		mid := (first + last) / 2
+		switch {
+		case src[mid] == find:
+			return mid
+		case src[mid] < find:
+			first = mid + 1
+		case src[mid] > find:
+			last = mid - 1
+		}
+	}
 	return -1
 }
