0453c1d2adae32a0580b835727fffb854b1914ca
user-penguin <kobzev96@gmail.com>
2022-04-08 03:41:21 +0700

    tdd style init p17

--- p17/problem-17.go
+++ p17/problem-17.go
@@ -3,6 +3,19 @@
 */
 package p17
 
-func Run() {
+import "fmt"
 
+func Run() {
+	source := []int{6, 1, 23, 33, -2, 5, 34, -15, 6, 2}
+	find := 2
+	index := binarySearch(source, find)
+	if index >= 0 {
+		fmt.Printf("Элемент %d найден, его позиция в массиве %d\n", find, index)
+	} else {
+		fmt.Printf("Элемент %d не найден", find)
+	}
+}
+
+func binarySearch(src []int, find int) int {
+	return -1
 }
