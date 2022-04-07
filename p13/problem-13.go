b4dd275ce816ebebb14da896c6aefdaa2f66f661
user-penguin <kobzev96@gmail.com>
2022-04-08 02:43:01 +0700

    some refactoring

--- p13/problem-13.go
+++ p13/problem-13.go
@@ -8,8 +8,24 @@ import (
 )
 
 func Run() {
-	a := 5
-	b := 13
-	a, b = a*b/a, a*b/b
+	spin1()
+	spin2()
+}
+
+func spin1() {
+	a, b := 5, 13
+	a, b = b, a
+	printAB(a, b)
+}
+
+func spin2() {
+	a, b := 6, 15
+	a = a * b // a = 90
+	b = a / b // b = 15
+	a = a / b // a = 6
+	printAB(a, b)
+}
+
+func printAB(a int, b int) {
 	fmt.Printf("a = %d, b = %d\n", a, b)
 }
