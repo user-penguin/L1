fd59bdac19f281b092c05a6568bce8846bbca075
user-penguin <kobzev96@gmail.com>
2022-04-08 20:48:34 +0700

    completed problem 8

--- p8/problem-8.go
+++ p8/problem-8.go
@@ -1,4 +1,4 @@
-/*
+/*Package p8
 Дана переменная int64. Разработать программу которая устанавливает i-й бит в 1 или 0
 */
 package p8
@@ -6,7 +6,6 @@ package p8
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,31 +16,36 @@ func Run() {
 	if err != nil {
 		fmt.Printf("Ошибка во время чтения: %s", err)
 	}
+
 	position, err := readInt("Какой бит будем менять? (Давайте считать с нулевого)")
 	if err != nil {
 		fmt.Printf("Ошибка во время чтения: %s", err)
 	}
+
 	bit, err := readInt("Что запишем в бит?")
 	if err != nil {
 		fmt.Printf("Ошибка во время чтения: %s", err)
 	}
+
+	// проверка, что в значение бита точно хотят установить 0 или 1
 	if bit < 0 || bit > 1 {
 		fmt.Printf("Значение бита может быть или 1, или 0, попробуйте ещё раз")
 		return
 	}
 
-	fmt.Printf("%d in binary = %64b\n", sourceNum, uint64(sourceNum))
+	fmt.Printf("Исходное число %d в прямом коде:\n %064b\n", sourceNum, uint64(sourceNum))
+	changedSrc := changeBit(sourceNum, position, bit)
+	fmt.Printf("Новое число %d в прямом коде:\n %064b\n", changedSrc, uint64(changedSrc))
+}
 
-	if bit == 1 {
-		// надо получить 000010
-		mask := pow2(position)
-		res := sourceNum | mask
-		fmt.Printf("Результат %064b %d\n", res, res)
-	} else {
-		// надо получить 111101
-		mask := math.MaxInt64 &^ pow2(position)
-		res := sourceNum & mask
-		fmt.Printf("Результат %064b %d\n", res, res)
+func changeBit(src int64, position int64, value int64) int64 {
+	tmp := uint64(src)
+	mask := uint64(pow2(position))
+	switch value {
+	case 0:
+		return int64(tmp &^ mask)
+	default:
+		return int64(tmp | mask)
 	}
 }
 
