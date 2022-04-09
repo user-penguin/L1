f748504f9bfe031e41b2cec6347bceb12688ac70
user-penguin <kobzev96@gmail.com>
2022-04-10 00:00:06 +0700

    few steps of p15

--- p15/problem-15.go
+++ p15/problem-15.go
@@ -15,22 +15,34 @@ func main() {
 */
 package p15
 
-import "fmt"
+import (
+	"log"
+	"math/rand"
+)
 
 var justString string
 
 func someFunc() {
-	v := createHugeString(1 << 10) //1024
-	justString = v[:100]
+	v := createHugeString(1 << 20) //1024
+
+	// никак не контролируется правая граница слайса, на примере с размером строки < 100, словим панику по выходу
+	// за границу
+	rightIndex := 100
+	switch {
+	case rightIndex <= len(v)-1:
+		justString = v[:100]
+	default:
+		log.Println("func SomeFunc(). Некорректная правая граница слайса, выход за пределы строки")
+	}
 }
 
+// Реализовал данную функцию, т.к. её не было, что приводило к ошибке компиляции
 func createHugeString(size int) string {
-	fmt.Println(size)
-	res := ""
-	for i := 0; i < size; i++ {
-		res += "a"
+	res := make([]byte, size)
+	for n := range res {
+		res[n] = byte(rand.Intn(122-48) + 48)
 	}
-	return res
+	return string(res)
 }
 
 func Run() {
